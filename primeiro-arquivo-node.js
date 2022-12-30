1737d31f12d129d71f6ab90bc2ee9ce1c8f5a821
Nathan Carlos Santos Lima <32680502+NathanCarlos@users.noreply.github.com>
2022-12-30 17:15:54 -0300

    Add files via upload

---
+++ primeiro-arquivo-node.js
@@ -0,0 +1,12 @@
+console.log("Primeiro Hello World!");
+
+const numero1 = 10;
+const numero2 = 15;
+
+const soma = () => {
+    return numero1 + numero2;
+};
+
+console.log('O resultado da soma é:', soma());
+
+console.log('A data de hoje é:', new Date());
